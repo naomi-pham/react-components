0dcf4b251332381a3cf59b20c9b9ce0a9d149060
naomipham <phambaonguyendn@gmail.com>
2023-05-19 22:59:13 +0700

    Fix error in throttle file

--- src/utils/throttle.jsx
+++ src/utils/throttle.jsx
@@ -1,11 +1,11 @@
 export function throttle(fn, delay = 300) {
-	return (args) => {
-		if (fn.id) return;
+  return (args) => {
+    if (fn.id) return
 
-		fn.id = setTimeout(() => {
-			fn.call(this, args);
-			clearTimeout(fn.id);
-			fn.id = null;
-		}, delay);
-	};
+    fn.id = setTimeout(() => {
+      fn.call(this, args)
+      clearTimeout(fn.id)
+      fn.id = null
+    }, delay)
+  }
 }
