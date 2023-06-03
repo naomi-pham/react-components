d5d17841c5d5d44eec007bb3489b223629ae39a1
naomipham <phambaonguyendn@gmail.com>
2023-06-03 11:19:47 +0700

    Clean up main branch

--- src/components/neumorphism/siren-order/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Nav } from './Nav'
-export { default as ProductInfo } from './ProductInfo'
-export { default as Selection } from './Selection'
-// export { default as Favorites } from "./Favorites";
